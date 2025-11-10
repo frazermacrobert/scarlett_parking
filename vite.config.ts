9bd40d49da1a5fdca97b23e5ee877c0b7d369687
frazermacrobert <frazermacrobert@gmail.com>
2025-11-10 13:23:22 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-   base: '/scarlett_parking/',  // match your repo name
+  base: '/scarlett_parking/',   // match the repo name
   plugins: [react()],
-  server: { port: 5173 },
   build: { outDir: 'dist' }
 })
+
